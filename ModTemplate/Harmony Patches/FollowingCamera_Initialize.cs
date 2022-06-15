19d75a2a4ea0752d0ac959f2bb8d1d58e21c915b
slch2o <101753869+slch2o@users.noreply.github.com>
2022-06-14 22:24:51 -0400

    Camera Mod Max
    Values have been updated for the "full" version of the camera mod

---
+++ ModTemplate/Harmony Patches/FollowingCamera_Initialize.cs
@@ -0,0 +1,28 @@
+﻿using BokuMono;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BabysFirstMod.Harmony_Patches
+{
+
+    [HarmonyPatch(typeof(FollowingCamera), nameof(FollowingCamera.Initialize))]
+    internal class FollowingCamera_Initialize
+    {
+        [HarmonyPostfix]
+        internal static void Postfix(FollowingCamera __instance)
+        {
+            float[] distarray = new float[7] { 21.5f, 18.5f, 15.5f, 12.5f, 9.5f, 7.5f, 81.5f };
+            float[] delayarray = new float[7] { 10.0f, 10.0f, 10.0f, 10.0f, 10.0f, 10.0f, 10.0f};
+            float[] stiffarray = new float[7] { 4.0f, 4.0f, 4.0f, 4.0f, 4.0f, 4.0f, 4.0f };
+            float[] fisharray = new float[7] { 0.0f, 0.0f, 2.0f, 4.0f, 6.0f, 8.0f, 0.0f };
+            __instance.distanceArray = distarray;
+            __instance.followDelayMaxDistanceArray = delayarray;
+            __instance.followDelayStiffnessArray = stiffarray;
+            __instance.followDelayFishingStiffnessArray = fisharray;
+        }
+    }
+}
---
+++ ModTemplate/Harmony Patches/GameController_Init.cs
@@ -0,0 +1,25 @@
+﻿using BokuMono;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BabysFirstMod.Harmony_Patches
+{
+
+    [HarmonyPatch(typeof(GameController), nameof(GameController.Init))]
+    internal class GameController_Init
+    {
+        [HarmonyPostfix]
+        internal static void Postfix(GameController __instance)
+        {
+            __instance.GameSetting.FieldObjectClipPadding = 3.0f;
+            __instance.GameSetting.FieldObjectFarClip = 150.0f;
+            __instance.GameSetting.FieldObjectSubtractedFarClip = 0.0f;
+            __instance.GameSetting.FieldObjectFarClipLimit = 2.0f;
+            __instance.GameSetting.FieldObjectFarClipSpeed = 0.2f;
+        }
+    }
+}
